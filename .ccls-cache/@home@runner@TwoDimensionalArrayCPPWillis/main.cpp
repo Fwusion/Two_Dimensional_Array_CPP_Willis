2f74bce745b810e6ceedbbf4360e9a0de8773f59
Isaiah Willis <102613759+Fwusion@users.noreply.github.com>
2022-04-06 21:49:10 +0000

    created a 2 dimensional array to hold the data for city and week. Changed the for loop to work with what I changed and added "system("clear") to clean up the run.

--- .ccls-cache/@home@runner@TwoDimensionalArrayCPPWillis/main.cpp
+++ .ccls-cache/@home@runner@TwoDimensionalArrayCPPWillis/main.cpp
@@ -1,8 +1,8 @@
-//Student Name:
+//Student Name: Isaiah Willis
 
 //Teacher: Dr. Tyson McMillan
 
-//Date:
+//Date: 04/06/2022
 
 //A program to practice working with multi-dimensional arrays
 
@@ -10,49 +10,6 @@
 
 using namespace std;
 
-/*
-
-Research Input data: go to http://www.weather.com (Links to an external site.)
-
-Pick two cities of your choice. Fill in the temperature for the 7 days of this week of both cities. Fill in this text data table, which will be used in your program.
-
-//City_One = 1, City_Two = 2  Replace City_One and City_Two with the names of your cities
-
-//Data Table
-
-//City X, Day X: Temp
-
-City 1, Day 1: TEMP
-
-City 1, Day 2: TEMP
-
-City 1, Day 3: TEMP
-
-City 1, Day 4: TEMP
-
-City 1, Day 5: TEMP
-
-City 1, Day 6: TEMP
-
-City 1, Day 7: TEMP
-
-City 2, Day 1: TEMP
-
-City 2, Day 2: TEMP
-
-City 2, Day 3: TEMP
-
-City 2, Day 4: TEMP
-
-City 2, Day 5: TEMP
-
-City 2, Day 6: TEMP
-
-City 2, Day 7: TEMP
-
-*/
-
- 
 
 int main()
 
@@ -62,19 +19,18 @@ int main()
 
       const int WEEK = 7;
 
+      int** forecast = new int*[CITY];
+    for(int i =0;i < CITY; i++)
+      {
+        forecast[i] = new int[WEEK];
+      }
+  
+  
+
+   
+   
+
  
-
-    int temperature[CITY][WEEK];
-
-    //Note your input data from the above
-
-    cout << "Enter all temperature for a week of first city and then second city. \n";
-
- 
-
-    // Inserting the values into the temperature array
-
-    //note for every dimension of the array you need a for loop 2 dimensions = 2 for loops
 
     for (int i = 0; i < CITY; ++i)
 
@@ -83,18 +39,22 @@ int main()
         for(int j = 0; j < WEEK; ++j)
 
         {
-
+           cout << "Enter all temperature for a week of first city and then second city. \n";
+          
             cout << "City " << i + 1 << ", Day " << j + 1 << " : ";
 
-            cin >> temperature[i][j];
+            cin >> forecast[i][j];
+
+          system("clear");
 
         }
 
     }
 
+
  
 
-    cout << "\n\nDisplaying Values:\n";
+  /* cout << "\n\nDisplaying Values:\n";
 
  
 
