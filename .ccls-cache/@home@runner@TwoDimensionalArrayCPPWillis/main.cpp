4dcfaf3e131b81b47a226bd4a22ab75aae3fa070
Isaiah Willis <102613759+Fwusion@users.noreply.github.com>
2022-04-06 23:02:42 +0000

    Created a nested for loop with an if statement inside outputting the specific city along with the arrays with temperatures.

--- .ccls-cache/@home@runner@TwoDimensionalArrayCPPWillis/main.cpp
+++ .ccls-cache/@home@runner@TwoDimensionalArrayCPPWillis/main.cpp
@@ -42,85 +42,34 @@ forecast[1][3] = 86;
 forecast[1][4] = 89;
 forecast[1][5] = 90;
 forecast[1][6] = 83;
-
-  
-  
-     /* const int CITY = 2;
-
-      const int WEEK = 7;
-
-      int** forecast = new int*[CITY];
-    for(int i =0;i < CITY; i++)
-      {
-        forecast[i] = new int[WEEK];
-      }
-  
-  
-
-   
    
 
+
+    cout << "\nApril 8th to 14th Weekly Forecast:" << endl;
  
 
-    for (int i = 0; i < CITY; ++i)
+    for (int i = 0; i < city; ++i)
 
     {
-
-        for(int j = 0; j < WEEK; ++j)
-
-        {
-           cout << "Enter all temperature for a week of first city and then second city. \n";
-          
-            cout << "City " << i + 1 << ", Day " << j + 1 << " : ";
-
-            cin >> forecast[i][j];
-
-          system("clear");
-
-        }
-
-    }
-
-    cout << "Your Citys' Weekly Forecast:" << endl;
- 
-
-    for (int i = 0; i < CITY; ++i)
-
-    {
-
-        for(int j = 0; j < WEEK; ++j)
+      cout << endl;
+        for(int j = 0; j < days; ++j)
 
         {
 
             if(i == 0)
             {
-              cout << "Arlington"
+              cout << "Arlington, ";
             }
+          else 
+            {
+              cout << "Austin, ";
+            }
+
+          cout << "Day " << i + 1 << " : " << forecast[i][j] << endl;
 
         }
 
     }
-
-
-  
-
-   /*For up to 5 Points Extra Credit
-
-      Expand this program:
-
-      1. Create a one dimensional array, cityOne[7] to store the 7 temperatures of city 1, assume that value 0 represents day 1, 1 represents day 2....
-
-      2. Create a on dimensional array, cityTwo[7] to store the 7 temperatures of city 2 , assume that value 0 represents day 1, 1 represents day 2....
-
-      3. Using a for loop (or nested for loop), feed the data of CityOne[] and CityTwo[] into temperature[i][j], where i is index of the City Number, and j is the index of the temp for the day.
-
-      4. print the results of temperature[i][j]; to the screen.
-
-    */
-
- 
-
- 
 
     return 0;
 
